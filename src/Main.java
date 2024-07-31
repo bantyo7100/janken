e331de5651f96d103c3fb035a779fe23cdc47373
haru_ban <h.yaegashi.sys23@morijyobi.ac.jp>
2024-07-31 19:15:09 +0900

    close #17, 18

--- src/Main.java
+++ src/Main.java
@@ -90,6 +90,18 @@ public class Main {
 
 
 
+//        //以降にテストコードを追加して検証を行う
+
+//        System.out.println("テスト結果");
+
+//        System.out.println(user.getHand().getHandName());
+
+
+
+        //comの手を生成
+
+        com.createRandomHand();
+
         //以降にテストコードを追加して検証を行う
 
 
